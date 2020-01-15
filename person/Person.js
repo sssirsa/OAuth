ca8fda53a3e5df591eb3a06a110912c6ec86c3a7
Eduardo Ivan Piña Gdc <dossosllac@gmail.com>
2020-01-15 14:04:30 -0600

    fix for ObjectID in persona

--- person/Person.js
+++ person/Person.js
@@ -6,12 +6,38 @@ const PersonSchema = new mongoose.Schema({
     apellido_materno: String,
     direccion: String,
     telefono: Number,
-    ife: String,
     foto: String,
-    sucursal_id: String,
-    user_id: String,
-    udn_id: String,
+    sucursal_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Agency' },
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    udn_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Subsidiary' },
     deleted: {type: Boolean, default: false}
 });
 
+const UserSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    password: String,
+    is_active:  {type: Boolean, default: true}
+});
+
+const AgencySchema = new mongoose.Schema({
+    agencia: String,
+    centro: String,
+    direccion: String,
+    telefono: String,
+    zona: String,
+    deleted: {type: Boolean, default: false}
+});
+
+const SubsidiarySchema = new mongoose.Schema({
+    nombre: String,
+    direccion: String,
+    telefono: String,
+    responsable: String,
+    deleted: {type: Boolean, default: false}
+});
+
+const User = mongoose.model('User', UserSchema);
+const Subsidiary = mongoose.model('Subsidiary', SubsidiarySchema);
+const Agency = mongoose.model('Agency',AgencySchema);
 module.exports = mongoose.model('Person', PersonSchema);
